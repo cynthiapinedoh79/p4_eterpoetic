8e9f750252042e9e7fab31f9c08b4ae686891f66
Cpinedo <cynthiapinedoh79@gmail.com>
2025-10-23 21:07:44 +0000

    urls.py blog app

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 # /workspaces/p4_eterpoetic/blog/views.py
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
 
 def home(request):
     return HttpResponse("Blog home OK")
