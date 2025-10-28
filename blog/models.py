7d181f595bcb7d8767ec7a9b82111847d4a6be0c
Cpinedo <cynthiapinedoh79@gmail.com>
2025-10-28 04:08:56 +0000

    added again env.py and added Comments model to blog app

--- blog/models.py
+++ blog/models.py
@@ -26,3 +26,28 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
+    
+class Comment(models.Model):
+    """
+    Stores a single comment entry related to :model:`auth.User`
+    and :model:`blog.Post`.
+    """
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, 
+        related_name="commenter",
+        null=True, blank=True,) # <-- TEMPORARY to allow migration
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+    
+
+
