a288e82c1af1b6bb84d2a2c5364e999844a93d97
Cpinedo <cynthiapinedoh79@gmail.com>
2025-10-29 06:09:47 +0000

    feature: def view of post_detail.html, adding default img

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,20 @@ from django.urls import reverse
 
 # Create your views here.
 class PostList(generic.ListView):
+    """
+    Returns all published posts in :model:`blog.Post`
+    and displays them in a page of six posts. 
+    **Context**
+
+    ``queryset``
+        All published instances of :model:`blog.Post`
+    ``paginate_by``
+        Number of posts per page.
+        
+    **Template:**
+
+    :template:`blog/index.html`
+    """
     queryset = Post.objects.filter(status=1)
     template_name = 'blog/index.html'
     paginate_by = 6
@@ -31,5 +45,6 @@ def post_detail(request, slug):
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {"post": post,
+         "coder": "Eterpoetic"},
     )
--- eterpoetic/settings.py
+++ eterpoetic/settings.py
@@ -41,7 +41,7 @@ else:
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
