7de9089b03d576d03f9890b254c9f32af30291d8
Cpinedo <cynthiapinedoh79@gmail.com>
2025-10-30 02:10:29 +0000

    feature: default or selected img on view post in blog page

--- eterpoetic/settings.py
+++ eterpoetic/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 
     # cloudinary
     'cloudinary_storage',
+    'cloudinary',
     # allauth
     'allauth',
     'allauth.account',
@@ -72,7 +73,6 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     #----------
     'django_summernote',
-    'cloudinary',
     'blog',
     'about',
 ]
