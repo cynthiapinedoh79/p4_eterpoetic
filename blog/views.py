cde30235cdb13a9217f59f45ec1aa40154212652
Cpinedo <cynthiapinedoh79@gmail.com>
2025-10-30 01:40:48 +0000

    feat: delete button for comments in posts

--- blog/views.py
+++ blog/views.py
@@ -96,3 +96,19 @@ def comment_edit(request, slug, comment_id):
 
     return redirect("blog:post_detail", slug=slug)
     
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return redirect("blog:post_detail", slug=slug)
