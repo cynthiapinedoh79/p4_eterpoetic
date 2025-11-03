d2150b7fa2174ff50b962ecb7d7a2fda783a8360
Cpinedo <cynthiapinedoh79@gmail.com>
2025-11-03 03:54:41 +0000

    updating poetry/models.py and html

--- poetry/urls.py
+++ poetry/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 app_name = "poetry"
+
 urlpatterns = [
     path("", views.poem_list, name="poetryhome"),
     # IMPORTANT: prefix detail so it doesn't match /about/ or /blog/
-    path("<slug:slug>/", views.poem_detail, name="poem_detail"),
+    path("poem/<slug:slug>/", views.poem_detail, name="poem_detail"),
 ]
 
