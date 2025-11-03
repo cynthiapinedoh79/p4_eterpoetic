a8d088c8076df1e2eb4904b02c57fb7875f2fdf0
Cpinedo <cynthiapinedoh79@gmail.com>
2025-11-03 01:59:29 +0000

    feat: added hover to Nav bar

--- poetry/urls.py
+++ poetry/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "poetry"
 urlpatterns = [
     path("", views.poem_list, name="poetryhome"),
-    path("<slug:slug>/", views.poem_detail, name="detail"),
+    # IMPORTANT: prefix detail so it doesn't match /about/ or /blog/
+    path("<slug:slug>/", views.poem_detail, name="poem_detail"),
 ]
 
