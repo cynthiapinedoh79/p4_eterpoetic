2b40a3c150406b33c2a950f335cf5c6a421544e0
Cpinedo <cynthiapinedoh79@gmail.com>
2025-11-04 20:40:09 +0000

    update wsgi.py, fix poem detail, add collection filter

--- eterpoetic/wsgi.py
+++ eterpoetic/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise  # <-- 1. Import Whitenoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eterpoetic.settings")
 
+# 2. Get the default application first
 application = get_wsgi_application()
+
+# 3. Wrap the default application with Whitenoise
+application = WhiteNoise(application)
