4aab01ef0901c27cf01711585fb0aed4a750b1b6
Cpinedo <cynthiapinedoh79@gmail.com>
2025-11-05 23:14:29 +0000

    Fix: separate style.css into Apps css files

--- eterpoetic/settings.py
+++ eterpoetic/settings.py
@@ -41,7 +41,7 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # ALLOWED_HOSTS for production (when DEBUG is False)
