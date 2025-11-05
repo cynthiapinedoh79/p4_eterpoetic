1aa4ebb69d3df8813a407a1cb515d322cf59b030
Cpinedo <cynthiapinedoh79@gmail.com>
2025-11-05 09:21:16 +0000

    Fix: remove duplicate banner, increase quote font size, and remove bottom margin

--- eterpoetic/settings.py
+++ eterpoetic/settings.py
@@ -41,7 +41,7 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # ALLOWED_HOSTS for production (when DEBUG is False)
