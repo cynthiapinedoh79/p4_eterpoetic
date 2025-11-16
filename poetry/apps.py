fd84941c5ba4318f3344c5aac746559fdc397423
Cpinedo <cynthiapinedoh79@gmail.com>
2025-11-16 21:17:37 +0000

    Done-ready to deploy

--- poetry/apps.py
+++ poetry/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 class PoetryConfig(AppConfig):
-    """App configuration for the Poetry app"""s
+    """App configuration for the Poetry app"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'poetry'
